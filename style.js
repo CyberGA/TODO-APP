ce3438a38903515e6282814602d729cd06d5a215
CyberGA <etomu.joshua@gmail.com>
2021-04-27 23:39:34 +0100

    added interactions to the DOM

--- style.js
+++ style.js
@@ -1,4 +1,46 @@
-const todo_checkbox = document.getElementsByClassName('todo-checkbox')
-const todo_list = document.getElementsByClassName('todo-list')
+// add complete & incomplete
+const lists = document.querySelector(".todo-lists");
 
+lists.addEventListener("click", function (e) {
+  if (e.target.tagName == "INPUT" && e.target.type == "checkbox") {
+    const li = e.target.previousElementSibling;
+    if (e.target.checked) {
+      li.classList.add("todo-complete");
+      lists.removeChild(li.parentElement);
+      lists.appendChild(li.parentElement);
+    } else {
+      li.classList.remove("todo-complete");
+      lists.prepend(li.parentElement);
+    }
+  }
+});
 
+// add todo functionality
+const addTodoForm = document.forms.add_todo;
+
+addTodoForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const newTodo = addTodoForm.firstElementChild.value;
+
+  // creating list
+  const div = document.createElement("DIV");
+  const li = document.createElement("LI");
+  const checkbox = document.createElement("INPUT");
+
+  //add classes
+  div.classList.add("todo-item");
+  li.classList.add("todo-list");
+  checkbox.classList.add("todo-checkbox");
+
+  //add textContents
+  li.textContent = newTodo;
+  checkbox.type = "checkbox";
+
+  // append to lists:
+  div.appendChild(li);
+  div.appendChild(checkbox);
+
+  if (li.textContent != "") {
+    lists.prepend(div);
+  }
+});
